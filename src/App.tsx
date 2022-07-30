927718a6b0728cafe63e6669194a88b3c4d48b0a
Helena Oliveira <helena@informach.com.br>
2022-07-30 05:07:09 -0300

    add linting

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,10 @@ import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
-
   return (
     <ThemeProvider theme={defaultTheme}>
       <h1>Hello World</h1>
       <GlobalStyle />
     </ThemeProvider>
-    
   )
 }
